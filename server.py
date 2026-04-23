2274cd37b13c053ea2c0b94f1bf6d4b3fa09dba2
Boluwatife66 <boluodunmoluwa@gmail.com>
2026-04-23 15:04:53 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import threading
+import os
+import time
+import urllib.request
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
@@ -8,12 +11,26 @@ class Handler(BaseHTTPRequestHandler):
         self.wfile.write(b"PepeRush Bot is running!")
     
     def log_message(self, format, *args):
-        pass  # suppress logs
+        pass
+
+def keep_alive():
+    url = os.environ.get("RENDER_EXTERNAL_URL", "")
+    if not url:
+        return
+    while True:
+        try:
+            urllib.request.urlopen(url)
+        except:
+            pass
+        time.sleep(600)  # ping every 10 minutes
 
 def run_server():
-    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(("0.0.0.0", port), Handler)
     server.serve_forever()
 
 def start():
-    thread = threading.Thread(target=run_server, daemon=True)
-    thread.start()
+    t1 = threading.Thread(target=run_server, daemon=True)
+    t1.start()
+    t2 = threading.Thread(target=keep_alive, daemon=True)
+    t2.start()
